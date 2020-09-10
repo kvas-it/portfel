45a84d14a2fea1358a72446fb63dfc1cdf2fc860
Vasily Kuznetsov <kvas.it@gmail.com>
2020-09-10 20:55:06 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
+import os
 from setuptools import setup
+
+readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(readme_path, 'r', encoding='utf-8') as f:
+    readme_md = f.read()
 
 setup(
     name='portfel',
     version='0.1.0',
     description='Portfolio analysis tools',
+    long_description=readme_md,
+    long_description_content_type='text/markdown',
     author='Vasily Kuznetsov',
     author_email='kvas.it@gmail.com',
     packages=[
